08ff1906a27d536a1a26ba010eb4a0c0a9e1a6c0
Hwangtaehun <r283@naver.com>
2022-06-23 22:31:56 +0900

    사막꾸미기 랑 카메라 fov
    카메라 구도와 사막 절벽 배치, 하늘 색상 변경을 통해서 사막을 꾸미기
    생각보다 뒷부분 설치가 어려웠다.

--- SnowSence/Intermediate/Build/Win64/SnowSenceEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30145
-#include "C:/Users/r283/Desktop/Github/2022-first-half-year-unreal/SnowSence/Intermediate/Build/Win64/SnowSenceEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
